5f679181c89818952c8eb08c66eb861d698f910d
keunsu <loverpc@gmail.com>
2022-01-26 21:17:09 +0900

    a

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -34,7 +34,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, "home.page.go.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
@@ -48,7 +48,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap["remote_ip"] = remoteIP
 
 	// send data to the template
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, "about.page.go.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
